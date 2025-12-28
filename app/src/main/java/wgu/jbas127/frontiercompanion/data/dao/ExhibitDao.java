dc3fe8369da72e23f8cb997d623e7c19e8a04a14
Joseph <jbas127@wgu.edu>
2025-12-28 13:33:15 -0500

    Add DAO methods

--- app/src/main/java/wgu/jbas127/frontiercompanion/data/dao/ExhibitDao.java
+++ app/src/main/java/wgu/jbas127/frontiercompanion/data/dao/ExhibitDao.java
@@ -2,9 +2,11 @@ package wgu.jbas127.frontiercompanion.data.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -15,17 +17,27 @@ public interface ExhibitDao {
     @Query("SELECT * FROM exhibit")
     LiveData<List<Exhibit>> getAllExhibits();
 
+    @Query("SELECT * FROM exhibit")
+    List<Exhibit> getAllExhibitsSync();
+
+
     @Query("SELECT * FROM exhibit WHERE id = :id")
     LiveData<Exhibit> getExhibitById(long id);
 
-    @Query("SELECT * FROM exhibit")
-    List<Exhibit> getAllExhibitsSync();
+    @Query("SELECT * FROM exhibit WHERE id = :id")
+    Exhibit getExhibitByIdSync(long id);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(List<Exhibit> exhibits);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insert(Exhibit exhibit);
+    long insert(Exhibit exhibit);
+
+    @Update
+    int update(Exhibit exhibit);
+
+    @Delete
+    int delete(Exhibit exhibit);
 
     @Query("SELECT * FROM exhibit WHERE " +
             "name LIKE '%' || :query || '%' OR " +
