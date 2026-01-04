03b24b7bdabb54e77891104e24c34b781cea0648
Joseph <jbas127@wgu.edu>
2026-01-03 19:49:31 -0500

    Add fragment/bottom nav

---
+++ app/src/main/java/wgu/jbas127/frontiercompanion/ui/activities/MainActivity.java
@@ -0,0 +1,31 @@
+package wgu.jbas127.frontiercompanion.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import wgu.jbas127.frontiercompanion.R;
+import wgu.jbas127.frontiercompanion.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+
+    }
+}
