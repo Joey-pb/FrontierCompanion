131f5c17f4b34f0721db3d8f4617821cd0de705f
Joseph <jbas127@wgu.edu>
2026-01-18 22:13:05 -0500

    Add getInstance()

--- frontierCompanion-android/app/src/main/java/wgu/jbas127/frontiercompanion/FrontierCompanionApplication.java
+++ frontierCompanion-android/app/src/main/java/wgu/jbas127/frontiercompanion/FrontierCompanionApplication.java
@@ -9,11 +9,18 @@ public class FrontierCompanionApplication extends Application {
     public AppDatabase db;
     public ExhibitRepository exhibitRepository;
 
+    private static FrontierCompanionApplication instance;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         db = AppDatabase.getInstance(this);
         exhibitRepository = new ExhibitRepository(this);
+        instance = this;
+    }
+
+    public static FrontierCompanionApplication getInstance() {
+        return instance;
     }
 }
