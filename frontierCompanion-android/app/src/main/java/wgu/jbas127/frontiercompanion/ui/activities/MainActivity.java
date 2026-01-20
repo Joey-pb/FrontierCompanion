b68e56b77b822070531ed7ae5d3531d5c77f1165
Joseph <jbas127@wgu.edu>
2026-01-20 17:01:25 -0500

    Enable edge-to-edge display

--- frontierCompanion-android/app/src/main/java/wgu/jbas127/frontiercompanion/ui/activities/MainActivity.java
+++ frontierCompanion-android/app/src/main/java/wgu/jbas127/frontiercompanion/ui/activities/MainActivity.java
@@ -3,7 +3,11 @@ package wgu.jbas127.frontiercompanion.ui.activities;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -21,9 +25,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(0,0,0, systemBars.bottom);
+            return insets;
+        });
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
